79a5a18588cfba0950a4411b790459bf8a8b45b2
Madjar Evan <evanmadjar@gmail.com>
2024-04-03 11:24:30 +0300

    feat: Implement simple admin panels to the models

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Category,
+    Product,
+    PriceItem,
+)
+
+
+class PriceItemInline(admin.TabularInline):
+    model = PriceItem
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+    )
+    inlines = [PriceItemInline]
