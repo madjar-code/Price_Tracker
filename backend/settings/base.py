cd0fc7ad21a22a012ed2d8ac58fc54cb3103ba0e
Madjar Evan <evanmadjar@gmail.com>
2024-04-04 21:13:32 +0300

    feat: Add default pagination to the project

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -72,6 +72,8 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
